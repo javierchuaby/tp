ab5a82f77710bebbadc0accbbea9a13122018efb
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-10-09 20:33:56 +0800

    Fix AttendanceCommandTest checkstyle

--- src/test/java/seedu/address/logic/commands/AttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AttendanceCommandTest.java
@@ -1,14 +1,13 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.AddressBook;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
