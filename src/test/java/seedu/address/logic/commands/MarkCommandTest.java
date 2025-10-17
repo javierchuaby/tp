bb25cea73c663be46eca5c2ed8d1d87c149c2a80
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2025-10-17 10:56:14 +0800

    Align MarkCommandTest expected tags with command behavior (no tag mutation)

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -10,9 +10,6 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
@@ -21,8 +18,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Points;
-import seedu.address.model.person.Tag;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for MarkCommand.
