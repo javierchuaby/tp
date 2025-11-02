aa97a3231bd8d002b06521acd8db7a42b615d9a1
jeroylim <138494063+jeroylim@users.noreply.github.com>
2025-11-02 23:40:01 +0800

    Fix untag handling

--- src/main/java/seedu/address/logic/commands/UntagCommand.java
+++ src/main/java/seedu/address/logic/commands/UntagCommand.java
@@ -37,10 +37,8 @@ public class UntagCommand extends Command {
 
     /** Success message template. */
     public static final String MESSAGE_SUCCESS = "Tag removed: %1$s";
-
     /** Error message when attempting to remove a tag that the person does not have. */
     public static final String MESSAGE_INVALID_TAG = "Invalid tag: %1$s";
-
     /** Error message when index is out of bounds. */
     private static final String MESSAGE_INVALID_INDEX = "The person index provided is invalid";
 
