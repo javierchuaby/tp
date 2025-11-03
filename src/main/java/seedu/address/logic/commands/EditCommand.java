96fb1aa741cc55abcabc291c534930b4e73428eb
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2025-11-03 16:31:33 +0800

    resolve more issues

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -3,9 +3,11 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FACULTY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_YEAROFSTUDY;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.Collections;
@@ -42,6 +44,8 @@ public class EditCommand extends Command {
             + "[" + PREFIX_NAME + "NAME] "
             + "[" + PREFIX_PHONE + "PHONE] "
             + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_YEAROFSTUDY + "YEAR] "
+            + "[" + PREFIX_FACULTY + "FACULTY] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " 1 "
@@ -84,6 +88,15 @@ public class EditCommand extends Command {
         if (editPersonDescriptor.getPhone().isPresent()) {
             for (Person existing : model.getClubTrack().getPersonList()) {
                 if (!existing.equals(personToEdit) && existing.getPhone().equals(editedPerson.getPhone())) {
+                    throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+                }
+            }
+        }
+
+        // If email is being changed, ensure no other person already uses the same email
+        if (editPersonDescriptor.getEmail().isPresent()) {
+            for (Person existing : model.getClubTrack().getPersonList()) {
+                if (!existing.equals(personToEdit) && existing.getEmail().equals(editedPerson.getEmail())) {
                     throw new CommandException(MESSAGE_DUPLICATE_PERSON);
                 }
             }
